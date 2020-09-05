0c91694663bd48f6ef638a3833eb14a753fc5a25
Polarbear08 <pistolstar0808@gmail.com>
2020-09-05 06:27:43 +0000

    initial commit

---
+++ src/main/java/com/example/demo/demo_polar/DemoPolarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.demo_polar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/demo_polar/DemoPolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.demo_polar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
